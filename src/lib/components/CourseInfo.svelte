27541dc910cd10947ad79c9ed33ec914ba7e40b9
Jay Joo <jj534@cornell.edu>
2023-02-26 11:42:10 -0500

    remove course desc

--- src/lib/components/CourseInfo.svelte
+++ src/lib/components/CourseInfo.svelte
@@ -6,11 +6,11 @@
 
 {#if course}
 	<div class="space-y-4">
-		<h2 class="text-4xl font-bold">{course?.name}</h2>
+		<h2 class="text-5xl font-bold">{course?.name}</h2>
 		<div>
-			<h4 class="mb-0">{course?.subtitle}</h4>
+			<h4 class="mb-0 text-base-content/80">{course?.subtitle}</h4>
 			{#if course?.aliases}
-				<p>
+				<p class="text-base-content/80">
 					Also known as:
 
 					{#each course?.aliases?.split(',') as alias}
@@ -20,19 +20,25 @@
 			{/if}
 		</div>
 
-		<div
-			class="cursor-pointer rounded-xl py-3 hover:bg-base-200"
-			on:click={() => {
-				isDescExpanded = !isDescExpanded;
-			}}
-		>
-			<p
-				class={`text-sm text-base-content/70 ${
-					isDescExpanded ? 'line-clamp-none' : 'line-clamp-3'
-				}`}
+		<!-- NOTE: course desc section removed because not relevant to problem OpinionHub is solving  -->
+		<!-- <div class="divider" />
+		<div>
+			<p class="text-lg font-bold">Description</p>
+			<div
+				class="cursor-pointer rounded-xl py-3 hover:bg-base-200"
+				on:click={() => {
+					isDescExpanded = !isDescExpanded;
+				}}
 			>
-				{course?.desc}
-			</p>
+				<p
+					class={`text-sm text-base-content/70 ${
+						isDescExpanded ? 'line-clamp-none' : 'line-clamp-3'
+					}`}
+				>
+					{course?.desc}
+				</p>
+			</div>
 		</div>
+		<div class="divider" /> -->
 	</div>
 {/if}
