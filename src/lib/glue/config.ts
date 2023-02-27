b8f17947eaa2e06e76b41b3fd9d14544884f861f
Jay Joo <jj534@cornell.edu>
2023-02-27 12:00:31 -0500

    add opinionhub sentry dsn

--- src/lib/glue/config.ts
+++ src/lib/glue/config.ts
@@ -5,4 +5,5 @@ export const PUBLIC_NAVS: IPublicNav[] = [];
 export const IS_GOOGLE_AUTH_ONLY = true;
 export const IS_ENFORCE_CORNELL_EMAIL = true;
 export const IS_BETA = true;
-export const SENTRY_DSN_PUBLIC = '';
+export const SENTRY_DSN_PUBLIC =
+	'https://7193745ed70c4e5899cc5637c442b19b@o4504752557457408.ingest.sentry.io/4504752560406528';
