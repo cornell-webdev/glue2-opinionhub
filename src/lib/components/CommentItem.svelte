f4041cb40ec908b19657384ee2b8b37fedcef0ef
Jay Joo <jj534@cornell.edu>
2023-02-27 18:46:17 -0500

    fix border not showing bug

--- src/lib/components/CommentItem.svelte
+++ src/lib/components/CommentItem.svelte
@@ -44,63 +44,65 @@
 </script>
 
 {#if comment}
-	<a href={isShowCourse ? `/course/${comment?.course}` : undefined}>
-		<div class="space-y-3 border-b border-base-content/20 pt-8 pb-7 last:border-b-0">
-			<div class="flex items-center justify-between">
-				<div class="flex items-center space-x-2">
-					<!-- course -->
-					{#if isShowCourse}
-						<a href="/course/{comment?.course}">
-							<button class="btn-primary btn-xs btn">
-								{comment?.expand?.course?.name}
-							</button>
-						</a>
-					{/if}
+	<div class="border-b border-base-content/20 last:border-b-0">
+		<a href={isShowCourse ? `/course/${comment?.course}` : undefined}>
+			<div class="space-y-3 pt-8 pb-7">
+				<div class="flex items-center justify-between">
+					<div class="flex items-center space-x-2">
+						<!-- course -->
+						{#if isShowCourse}
+							<a href="/course/{comment?.course}">
+								<button class="btn-primary btn-xs btn">
+									{comment?.expand?.course?.name}
+								</button>
+							</a>
+						{/if}
 
-					<!-- rating -->
-					<div class="rating rating-sm">
-						{#each [1, 2, 3, 4, 5] as stars}
-							<input
-								type="radio"
-								name={`comment-rating-${comment?.id}`}
-								class="mask mask-star-2 bg-yellow-400"
-								checked={stars ===
-									sentimentToStars({
-										sentimentScore: comment?.sentimentScore,
-										sentimentMagnitude: comment?.sentimentMagnitude
-									})}
-							/>
-						{/each}
+						<!-- rating -->
+						<div class="rating rating-sm">
+							{#each [1, 2, 3, 4, 5] as stars}
+								<input
+									type="radio"
+									name={`comment-rating-${comment?.id}`}
+									class="mask mask-star-2 bg-yellow-400"
+									checked={stars ===
+										sentimentToStars({
+											sentimentScore: comment?.sentimentScore,
+											sentimentMagnitude: comment?.sentimentMagnitude
+										})}
+								/>
+							{/each}
+						</div>
+
+						<!-- author nickname -->
+						<p class="text-sm text-base-content/80">{comment?.providerData?.author}</p>
 					</div>
-
-					<!-- author nickname -->
-					<p class="text-sm text-base-content/80">{comment?.providerData?.author}</p>
 				</div>
-			</div>
-			<p
-				class={`cursor-pointer whitespace-pre-line ${
-					isExpanded ? 'line-clamp-none' : 'line-clamp-3'
-				}`}
-				on:click={() => {
-					isExpanded = !isExpanded;
-				}}
-			>
-				{comment?.content}
-			</p>
-			<div class="flex items-center justify-end space-x-3">
-				<div>
-					<p class="text-sm text-base-content/80">
-						{formatDistanceToNowStrict(new Date(comment?.providerCreated))} ago
-					</p>
-				</div>
-				<RequireAuthButton
-					class="btn-success btn-outline btn-xs btn gap-1 rounded-xl"
+				<p
+					class={`cursor-pointer whitespace-pre-line ${
+						isExpanded ? 'line-clamp-none' : 'line-clamp-3'
+					}`}
 					on:click={() => {
-						toggleHelpfulComment(comment?.id);
+						isExpanded = !isExpanded;
 					}}
-					><IconUpArrow /> {comment?.upvotes} helpful
-				</RequireAuthButton>
+				>
+					{comment?.content}
+				</p>
+				<div class="flex items-center justify-end space-x-3">
+					<div>
+						<p class="text-sm text-base-content/80">
+							{formatDistanceToNowStrict(new Date(comment?.providerCreated))} ago
+						</p>
+					</div>
+					<RequireAuthButton
+						class="btn-success btn-outline btn-xs btn gap-1 rounded-xl"
+						on:click={() => {
+							toggleHelpfulComment(comment?.id);
+						}}
+						><IconUpArrow /> {comment?.upvotes} helpful
+					</RequireAuthButton>
+				</div>
 			</div>
-		</div>
-	</a>
+		</a>
+	</div>
 {/if}
