7c1921cc492a3112e5c89a3496f9b0ea11fd3b2b
Jay Joo <jj534@cornell.edu>
2023-02-27 18:22:57 -0500

    only render 3 per page on mobile

--- src/lib/components/RecentOpinions.svelte
+++ src/lib/components/RecentOpinions.svelte
@@ -1,16 +1,15 @@
 <script>
 	import { pb } from '$lib/glue/pocketbase';
 	import IconDownKarat from '$lib/icons/glue/IconDownKarat.svelte';
-	import { onMount } from 'svelte';
 	import CommentItem from './CommentItem.svelte';
 
 	let comments = [];
 	let page = 1;
 
-	const fetchComments = async () => {
+	const fetchComments = async (page) => {
 		try {
 			const fetchedComments = (
-				await pb.collection('comments').getList(page, 5, {
+				await pb.collection('comments').getList(page, 6, {
 					sort: '-providerCreated',
 					expand: 'course'
 				})
@@ -19,24 +18,29 @@
 		} catch (error) {}
 	};
 
-	onMount(() => {
-		fetchComments();
-	});
+	$: fetchComments(page);
 </script>
 
 <div class="space-y-2">
 	<h1 class="text-2xl font-bold">Recent opinions</h1>
-	<div>
+
+	<div class="hidden md:block">
 		{#each comments as comment (comment?.id)}
 			<CommentItem {comment} isShowCourse={true} />
 		{/each}
 	</div>
+
+	<div class="block md:hidden">
+		{#each comments?.slice(0, page * 3) as comment (comment?.id)}
+			<CommentItem {comment} isShowCourse={true} />
+		{/each}
+	</div>
+
 	<div class="flex justify-center">
 		<button
 			class="btn-primary btn gap-1 rounded-full"
 			on:click={() => {
 				page += 1;
-				fetchComments();
 			}}
 		>
 			<span class="text-2xl"><IconDownKarat /></span> Show more
